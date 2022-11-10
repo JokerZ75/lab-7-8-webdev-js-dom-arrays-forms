499d3c6e2236c21aa61f040d747cc285016450fd
Deacon Hughes <deaconhughes07@gmail.com>
2022-11-10 17:59:35 +0000

    Exchange rate done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-(function () {
-  let url = window.location.href;
-  let filename = url.split("/").pop();
-  let allfiles = document.querySelectorAll("nav ul li");
-  for (let index = 0; index < allfiles.length; index++) {
-    let openFile = allfiles[index].innerHTML;
-    openFile = openFile.split('"');
-    if (filename == openFile[1]) {
-      document
-        .querySelector("nav ul li:nth-child(" + [index + 1] + ")")
-        .setAttribute("class", "currentPage");
-    }
-  }
-  //   document.querySelector("nav ul li").setAttribute("class", "currentPage");
-})();
+
 
 document.getElementById("myTestEvent").addEventListener("click", function () {
   console.info("hi");
